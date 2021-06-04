1dd9300c78a1790a8f3e5cefe2f73eca00e13bda
Nestori Syynimaa <31915053+NestoriSyynimaa@users.noreply.github.com>
2021-06-04 10:39:11 +0300

    Delete training/TechMentor/Seattle 2018 directory

--- training/TechMentor/Seattle 2018/M02/Setup/1-DC/01-DC-Configure-part2.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Create KDS Root Key
-Add-KdsRootKey -EffectiveTime (Get-Date).AddHours(-10)
-
-# Download the certificate for later use
-$CertName="star.myo365.site.pfx"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-wget "https://github.com/Gerenios/public/blob/master/training/TechMentor/Seattle%202018/M02/$CertName\?raw=true" -OutFile "$env:TEMP\$CertName"
